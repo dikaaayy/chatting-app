a4cc0b89afdf4af7c271053b6bb5a9857ff96153
dikaaayy <andikayudhistira3@gmail.com>
2022-05-09 20:48:05 +0700

    creating page when there is no message yet

---
+++ client/src/components/MainContainer/Blank.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Blank() {
+  return (
+    <>
+      <p className="text-white font-bold text-6xl">There is no message yet</p>
+      <p className="font-semibold text-3xl text-[#818181]">Start Chatting Now!</p>
+    </>
+  );
+}
--- client/src/components/MainContainer/MainContainer.jsx
+++ client/src/components/MainContainer/MainContainer.jsx
@@ -4,6 +4,7 @@ import BottomBar from "./BottomBar";
 import Message from "./Message";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { messageListState, userInfoState } from "../../atom/Detail";
+import Blank from "./Blank";
 
 export default function MainContainer({ socket }) {
   const [messageList, setMessageList] = useRecoilState(messageListState);
@@ -19,12 +20,17 @@ export default function MainContainer({ socket }) {
   return (
     <div className="content-container">
       <TopNavigation />
-      <div className="content-list scrollbar-hide">
-        <Message username="Leon" timestamp="one week ago" message={`Lorem ipsum dolor. `} />
-        {messageList.map((message) => {
-          return <Message key={message.id} username={message.sender_name} timestamp={message.time} message={message.message} image_url={userInfo.image_url} />;
-        })}
-      </div>
+      {messageList.length === 0 ? (
+        <div className="ml-10 flex flex-col justify-center h-screen select-none gap-y-4">
+          <Blank />
+        </div>
+      ) : (
+        <div className="content-list scrollbar-hide">
+          {messageList.map((message) => {
+            return <Message key={message.id} username={message.sender_name} timestamp={message.time} message={message.message} image_url={userInfo.image_url} />;
+          })}
+        </div>
+      )}
       <div className="flex justify-center absolute left-0 bottom-0 w-full h-16 items-center bg-gradient-to-t from-gray-800 to-transparent">
         <BottomBar socket={socket} />
       </div>
